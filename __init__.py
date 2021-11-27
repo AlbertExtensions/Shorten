a333b2774543d802f0c90b52e5c03d79ab4be11a
Nikos Koukis <nickkouk@gmail.com>
2021-11-27 20:25:31 +0000

    Use albert api v0.4, do various improvements
    * Abide to albert api v0.4
    * Add fat try-except statement to inform the user in case of error
    * Make queries to server lazily - do not use ClipAction which does the
      request immediately but instead do the request only on <ENTER>
        * Add dependency to xclip for this
    * Add default item if no input is given to inform the user accordingly
    
    Signed-off-by: Nikos Koukis <nickkouk@gmail.com>

--- __init__.py
+++ __init__.py
@@ -1,56 +1,81 @@
 """Shorten link and copy to clipboard"""
 import time
-
-from albertv0 import *
-import json
-import requests
+import traceback
 from os import path
+import subprocess
 
+import requests
+from albert import *
 
-__iid__ = "PythonInterface/v0.2"
-__prettyname__ = "Shorten"
-__version__ = "0.1"
-__trigger__ = "short "
+__title__ = "Shorten"
+__version__ = "0.4.0"
+__triggers__ = "short "
 __author__ = "Bharat kalluri"
-__dependencies__ = ["requests"]
+__exec_deps__ = ["xclip"]
+__py_deps__ = ["requests", "traceback"]
+__homepage__ = "https://github.com/AlbertExtensions/Shorten"
 
 isgd_url = "https://is.gd/create.php?format=simple&url={}"
 
 icon_path = "{}/icons/{}.svg".format(path.dirname(__file__), "link")
 
 
-def get_short_url(input_url):
-    # TODO: Handle errors
+def get_short_url(input_url: str):
     api_resp = requests.get(isgd_url.format(input_url))
     return api_resp.content.decode()
+
+
+def copy_to_clipboard(s: str):
+    subprocess.run(f"echo {s} | xclip -rmlastnl -selection clipboard", shell=True)
 
 
 def handleQuery(query):
     results = []
 
-    if query.isTriggered and query.string.strip():
+    try:
 
-        # avoid rate limiting
-        time.sleep(0.3)
-        if not query.isValid:
-            return
+        if query.isTriggered and query.string.strip():
+            if len(query.string) >= 4:
+                results.append(
+                    Item(
+                        id=__title__,
+                        icon=icon_path,
+                        text="Click to copy shortened url to clipboard",
+                        subtext="Link shortened by is.gd",
+                        completion=__triggers__,
+                        actions=[
+                            FuncAction(
+                                "Copy URL to clipboard",
+                                lambda url=query.string: copy_to_clipboard(get_short_url(url)),
+                            )
+                        ],
+                    )
+                )
+            else:
+                results.append(
+                    Item(
+                        id=__title__,
+                        icon=icon_path,
+                        completion=__triggers__,
+                        text="Paste the URL that you want to shorten",
+                        actions=[],
+                    )
+                )
 
-        item = Item(
-            id=__prettyname__,
-            icon=icon_path,
-            completion=query.rawString,
-            text=__prettyname__,
-            actions=[]
+    except Exception:  # user to report error
+        results.insert(
+            0,
+            Item(
+                id=__title__,
+                icon=icon_path,
+                text="Something went wrong! Press [ENTER] to copy error and report it",
+                actions=[
+                    ClipAction(
+                        f"Copy error - report it to {__homepage__[8:]}",
+                        f"{traceback.format_exc()}",
+                    )
+                ],
+            ),
         )
 
-        if len(query.string) >=4:
-            return Item(
-                    id=__prettyname__,
-                    icon=icon_path,
-                    text="Click to copy shortened url to clipboard",
-                    subtext="Link shortened by is.gd",
-                    actions=[ClipAction("Copy URL to clipboard", get_short_url(query.string))])
-        else:
-            item.subtext = "Search in Stackoverflow!"
-            return item
     return results
